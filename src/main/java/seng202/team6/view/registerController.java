ad4815f7bee01d0d2071c2c498a654a6981847b2
Dana Lambert <dla72@uclive.ac.nz>
2018-08-30 23:26:02 +1200

    Updated Sign In/Register GUI

--- src/main/java/seng202/team6/view/registerController.java
+++ src/main/java/seng202/team6/view/registerController.java
@@ -1,33 +1,16 @@
 package seng202.team6.view;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ChoiceBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class SignInController {
-
-    @FXML
-    private ChoiceBox<String> signInSelection;
-
-    @FXML // This method is called by the FXMLLoader when initialization is complete
-    void initialize() {
-        ObservableList<String> availableChoices = FXCollections.observableArrayList("lem72", "rch141", "gon12", "dla72", "spl8");
-        signInSelection.setItems(availableChoices);
-    }
-
-    public void login() {
-        String userProfile = signInSelection.getSelectionModel().getSelectedItem();
-        System.out.println("User signed in: " + userProfile);
-    }
+public class registerController {
 
     @FXML
     public void toStartScreen(ActionEvent event) throws IOException {
@@ -38,5 +21,11 @@ public class SignInController {
         appStage.setScene(loginScene);
         appStage.show();
     }
+
+    public void createNewUser() {
+        System.out.println("Created a new user!!");
+
+    }
+
 
 }
