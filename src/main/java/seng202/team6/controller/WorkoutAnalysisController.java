37d6c6b365cb98ff7df63fab818567369e6d8c3e
Sean <spl48@uclive.ac.nz>
2018-09-19 20:26:06 +1200

    Cleaned up workout analysis controller

--- src/main/java/seng202/team6/controller/WorkoutAnalysisController.java
+++ src/main/java/seng202/team6/controller/WorkoutAnalysisController.java
@@ -56,19 +56,7 @@ public class WorkoutAnalysisController extends WorkoutsNavigator {
         ObservableList<String> availableChoices = FXCollections.observableArrayList("Heart Rate", "Distance", "Elevation", "Calories");
         activityTypeSelection.setItems(availableChoices);
         activityTypeSelection.getSelectionModel().select("Heart Rate");
-        /*
-        Activity testRun1 = makeTestRun1();
-        Activity testRun2 = makeTestRun2();
-        activities.add(testRun1);
-        activities.add(testRun2);
-        */
         activities = databaseManager.getActivities(ApplicationManager.getCurrentUserID());
-        /*
-        ObservableList<String> availableActivities = FXCollections.observableArrayList(testRun1.getDate().toString(), testRun2.getDate().toString());
-        System.out.println(availableActivities);
-        System.out.println(testRun1.getDate().toString().getClass().getName());
-        activitySelection.getSelectionModel().select(testRun1.getDate().toString());
-        */
         ObservableList<String> availableActivities = FXCollections.observableArrayList();
         for (Activity activity : activities){
             availableActivities.add(activity.getDate().toString());
@@ -152,54 +140,5 @@ public class WorkoutAnalysisController extends WorkoutsNavigator {
     		ApplicationManager.displayPopUp("YA DINGUSS!", "Must compare different activities and same data type ya dinguss", "error");
     	}
     }
-/*
-    private Activity makeTestRun1() {
-        LocalDate inputDate = LocalDate.of(2018, 10, 9);
-        LocalTime time1 = LocalTime.of(5, 30);
-        LocalTime time2 = LocalTime.of(5, 40);
-        LocalTime time3 = LocalTime.of(5, 45);
-        LocalTime time4 = LocalTime.of(5, 55);
-        LocalTime time5 = LocalTime.of(6, 10);
-        LocalTime time6 = LocalTime.of(6, 15);
-        Activity testActivity = new Activity("Running", inputDate, time1, time6, 4.00, 80, 120);
-        ActivityDataPoint p1 = new ActivityDataPoint(time1, 85, -43.530029, 172.582520, 88);
-        ActivityDataPoint p2 = new ActivityDataPoint(time2, 120, -43.523584, 172.579179, 100);
-        ActivityDataPoint p3 = new ActivityDataPoint(time3, 111, -43.519975, 172.579222, 94);
-        ActivityDataPoint p4 = new ActivityDataPoint(time4, 104, -43.522371, 172.589474, 88);
-        ActivityDataPoint p5 = new ActivityDataPoint(time5, 101, -43.530834, 172.586771, 88);
-        ActivityDataPoint p6 = new ActivityDataPoint(time6, 98, -43.530029, 172.582520, 92);
-        testActivity.addActivityData(p1);
-        testActivity.addActivityData(p2);
-        testActivity.addActivityData(p3);
-        testActivity.addActivityData(p4);
-        testActivity.addActivityData(p5);
-        testActivity.addActivityData(p6);
-        return testActivity;
-    }
-
-    private Activity makeTestRun2() {
-        LocalDate inputDate = LocalDate.of(2018, 10, 12);
-        LocalTime time1 = LocalTime.of(12, 30);
-        LocalTime time2 = LocalTime.of(12, 35);
-        LocalTime time3 = LocalTime.of(12, 55);
-        LocalTime time4 = LocalTime.of(13, 8);
-        LocalTime time5 = LocalTime.of(13, 17);
-        LocalTime time6 = LocalTime.of(13, 36);
-        Activity testActivity = new Activity("Running", inputDate, time1, time6, 4.00, 80, 120);
-        ActivityDataPoint p1 = new ActivityDataPoint(time1, 89, -43.530029, 172.582520, 88);
-        ActivityDataPoint p2 = new ActivityDataPoint(time2, 88, -43.523584, 172.579179, 107);
-        ActivityDataPoint p3 = new ActivityDataPoint(time3, 93, -43.519975, 172.579222, 19);
-        ActivityDataPoint p4 = new ActivityDataPoint(time4, 120, -43.522371, 172.589474, 32);
-        ActivityDataPoint p5 = new ActivityDataPoint(time5, 118, -43.530834, 172.586771, 50);
-        ActivityDataPoint p6 = new ActivityDataPoint(time6, 98, -43.530029, 172.582520, 76);
-        testActivity.addActivityData(p1);
-        testActivity.addActivityData(p2);
-        testActivity.addActivityData(p3);
-        testActivity.addActivityData(p4);
-        testActivity.addActivityData(p5);
-        testActivity.addActivityData(p6);
-        return testActivity;
-    }
-    */
 }
 
