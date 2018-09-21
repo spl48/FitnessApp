79ed6c855bc92d4f47a1774e0091da8e5639a347
lem72 <lem72@uclive.ac.nz>
2018-09-21 23:55:05 +1200

    Added "other" type to get activity type and other minor activity analysis changes

--- src/main/java/seng202/team6/analysis/ActivityAnalysis.java
+++ src/main/java/seng202/team6/analysis/ActivityAnalysis.java
@@ -2,8 +2,6 @@ package seng202.team6.analysis;
 
 import seng202.team6.models.Activity;
 import seng202.team6.models.ActivityDataPoint;
-import seng202.team6.models.User;
-
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -18,9 +16,9 @@ public class ActivityAnalysis {
     private static ArrayList<String> walkingWords = new ArrayList<String>(Arrays.asList("walk", "walking", "hike", "hiking"));
     private static ArrayList<String> cyclingWords = new ArrayList<String>(Arrays.asList("bike", "biking", "cycle", "cycling"));
 
-    public static double findMaximumHeartRate(Activity activity) {
-        double maxHeartRate = 0;
-        double currentHeartRate;
+    public static int findMaximumHeartRate(Activity activity) {
+        int maxHeartRate = 0;
+        int currentHeartRate;
 
         for (ActivityDataPoint dataPoint : activity.getActivityData()) {
             currentHeartRate = dataPoint.getHeartRate();
@@ -32,14 +30,17 @@ public class ActivityAnalysis {
         return maxHeartRate;
     }
 
-    public static double findMinimumHeartRate(Activity activity) {
+    public static int findMinimumHeartRate(Activity activity) {
+        if (activity.getActivityData().size() == 0) {
+            return 0;
+        }
         ArrayList<ActivityDataPoint> dataPoints = activity.getActivityData();
-        double minHeartRate = dataPoints.get(0).getHeartRate();
-        double currentHeartRate;
+        int minHeartRate = dataPoints.get(0).getHeartRate();
+        int currentHeartRate;
 
         for (ActivityDataPoint dataPoint : dataPoints) {
             currentHeartRate = dataPoint.getHeartRate();
-            if (currentHeartRate < minHeartRate) {
+            if (currentHeartRate < minHeartRate && currentHeartRate > 0) {
                 minHeartRate = currentHeartRate;
             }
         }
@@ -48,33 +49,16 @@ public class ActivityAnalysis {
     }
 
 
-    public double findStepCount(Activity activity, double strideLength) {
-        //todo change this to only count run and walk steps!
-        System.out.println(activity.getType());
-        int finalIndex = activity.getActivityData().size();
-        double distance = findDistanceFromStart(activity, finalIndex - 1);
-        System.out.println(distance / (strideLength * 0.0003048));
-        return (distance / (strideLength * 0.0003048));
-    }
-
-
-    public double findTotalDistance(Activity activity) {
-        double latitudeDist;
-        double longitudeDist;
-        double totalDistance = 0;
-        ArrayList<ActivityDataPoint> dataPoints = activity.getActivityData();
-        int arrayLength = activity.getActivityData().size();
-        int currentIndex = 0;
-
-        for (currentIndex = 0; currentIndex < arrayLength; currentIndex++) {
-            latitudeDist = abs(dataPoints.get(currentIndex + 1).getLatitude() - dataPoints.get(currentIndex).getLatitude());
-            longitudeDist = abs(dataPoints.get(currentIndex + 1).getLongitude() - dataPoints.get(currentIndex).getLongitude());
-
-            totalDistance = totalDistance + latitudeDist + longitudeDist;
+    public static double findStepCount(Activity activity, double strideLength) {
+        if (activity.getType() == "Walking" || activity.getType() == "Running") {
+            int finalIndex = activity.getActivityData().size();
+            double distance = findDistanceFromStart(activity, finalIndex - 1);
+            return (distance / (strideLength * 0.0003048));
         }
 
-        return totalDistance;
+        return 0;
     }
+
 
     /** Finds the total distance covered from the start of an activity
      * to a particular activity point at an index in that same activity
@@ -83,7 +67,7 @@ public class ActivityAnalysis {
      * @param index the index for the activity point the distance is being calculated up to.
      * @return a double represting the distance covered
      */
-    public double findDistanceFromStart(Activity activity, int index) {
+    public static double findDistanceFromStart(Activity activity, int index) {
 
         double totalDistance = 0;
         int currentIndex = 0;
@@ -121,78 +105,51 @@ public class ActivityAnalysis {
             return (rad * 180 / Math.PI);
         }
 
-    public double findCaloriesBurned(Activity activity, User user) {
+
+    public static double findCaloriesBurnedFromStart(double activityTime, String activityType, double userWeight) {
         double metValue;
         double calories;
-        double userWeight = user.getWeight();
-        long activityTime = activity.getTotalTime();
-        String activityType = activity.getType();
 
-        if (activityType == "Walking") {
-            metValue = 4.3;
+        if (activityType == "Cycling") {
+            metValue = 6;
+        } else if (activityType == "Walking") {
+            metValue = 3;
         } else if (activityType == "Running") {
-            metValue = 9.8;
+            metValue = 7.5;
         } else {
-            metValue = 8;
+            metValue = 4.5;             // Unknown (other) activity, average met Value
         }
 
-        calories = metValue * 3.5 * userWeight / 200 * activityTime / 1000;
+        calories = metValue * (3.5 * userWeight / 200 * activityTime);
 
+        if (calories < 0) {
+            return 0;
+        }
         return calories;
     }
 
-    public double findCaloriesBurnedFromStart(double time, double heartRate, User user) {
 
-        double calories = 0;
-        double userWeight = user.getWeight();
-        String gender = user.getGender();
-        double userAge = user.getAge();
-
-        if (gender == "Male") {
-            calories = ((userAge * 0.2017) - (userWeight * 0.09036) + (heartRate * 0.6309) - 55.0969) * time / 4.184;
-        } else {
-            calories = ((userAge * 0.074) - (userWeight * 0.05741) + (heartRate * 0.4472) - 20.4022) * time / 4.184;
-        }
-
-        return calories;
-    }
-
-    public static double calculateAverageSpeed(Activity activity) {
-        return (activity.getTotalTime() / 60) / (activity.getDistance());
-    }
 
 
     public static String getActivityType (Activity activity){
 
-        String activityType = null;
+
         String activityDescription = activity.getDescription();
         String [] words = activityDescription.split(" ");
-        double averageSpeed = calculateAverageSpeed(activity);
+
 
         for (String word : words) {
             if (runningWords.contains(word.toLowerCase())) {
-                activityType = "Running";
+                return "Running";
             } else if (walkingWords.contains(word.toLowerCase())) {
-                activityType = "Walking";
+                return "Walking";
             } else if (cyclingWords.contains(word.toLowerCase())) {
-                activityType = "Cycling";
+                return "Cycling";
             }
         }
 
-        if (activityType == null) {
-            int brisk_walking_pace = 5;
-            int fast_running_pace = 12;
+        return "Other";
 
-            if (averageSpeed <= brisk_walking_pace) {
-                activityType = "Walking";
-            } else if (averageSpeed <= fast_running_pace) {
-                activityType = "Running";
-            } else {
-                activityType = "Cycling";
-            }
-        }
-
-        return activityType;
 
     }
 }
