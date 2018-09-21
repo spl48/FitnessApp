0a817dbf2bd401f415cf40cbc9ae524ae74e94d7
Dana Lambert <dla72@uclive.ac.nz>
2018-09-22 01:31:46 +1200

    Activity Edit fields on Raw Data Screen.

--- src/main/java/seng202/team6/controller/EditRawDataController.java
+++ src/main/java/seng202/team6/controller/EditRawDataController.java
@@ -4,20 +4,20 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.Event;
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.ImageView;
 import seng202.team6.datahandling.DatabaseManager;
-import seng202.team6.models.Activity;
 import seng202.team6.models.ActivityDataPoint;
 import seng202.team6.models.User;
 
-import javafx.scene.image.ImageView;
-
-import javax.activity.ActivityCompletedException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-public class RawDataController2 extends WorkoutsNavigator{
+public class EditRawDataController extends WorkoutsNavigator {
 
     @FXML
     private TableView rawDataTable;
@@ -37,8 +37,6 @@ public class RawDataController2 extends WorkoutsNavigator{
     @FXML
     private ListView activitySelect;
 
-    private static ListView activitySelectEdit;
-
     @FXML
     private ImageView filterButton;
 
@@ -46,9 +44,17 @@ public class RawDataController2 extends WorkoutsNavigator{
 
     public void initialize() throws SQLException {
         User currUser = dbManager.getUser(ApplicationManager.getCurrentUserName());
-        ObservableList<Integer> activityList = FXCollections.observableArrayList(dbManager.getActivityIDs(currUser.getUserID()));
-        activitySelect.setItems(activityList);
-        setupTable();
+        activitySelect = RawDataController2.getActivitySelector();
+        //setupTable();
+        //showActivity();
+
+
+
+    }
+
+    @FXML
+    public void updateActivity() {
+
     }
 
     private TableColumn make_column(String name, String attributeName, int size) {
@@ -82,18 +88,6 @@ public class RawDataController2 extends WorkoutsNavigator{
         for (ActivityDataPoint record : records) {
             addRecordToTable(record);
         }
-
-        descriptionLabel.setText(dbManager.getActivity((int) activitySelect.getSelectionModel().getSelectedItem()).getDescription());
-        dateLabel.setText(dbManager.getActivity((int) activitySelect.getSelectionModel().getSelectedItem()).getStartDate().toString());
-        typeLabel.setText(dbManager.getActivity((int) activitySelect.getSelectionModel().getSelectedItem()).getType());
-        notesLabel.setText(dbManager.getActivity((int) activitySelect.getSelectionModel().getSelectedItem()).getNotes());
-
-        activitySelectEdit = activitySelect;
-
-    }
-
-    public static ListView getActivitySelector() {
-        return activitySelectEdit;
     }
 
     @FXML
@@ -109,14 +103,5 @@ public class RawDataController2 extends WorkoutsNavigator{
     @FXML
     public void darkenFilter(Event event) {
 
-    }
-
-    /**
-     * Navigates the user to the Workouts Splash Screen.
-     * @param event When the user clicks on a button directing to this screen.
-     */
-    @FXML
-    public void toActivityEditScreen(Event event) {
-        changeScreen(event, "/seng202/team6/view/RawDataVeiwerEditing.fxml");
     }
 }
