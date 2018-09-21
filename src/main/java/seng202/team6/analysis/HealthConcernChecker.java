bdb5ba4e26d89ffe2b99fa05710fe27b18d66729
lem72 <lem72@uclive.ac.nz>
2018-09-21 18:37:13 +1200

    Made healthConcernChecker javadoc better

--- src/main/java/seng202/team6/analysis/HealthConcernChecker.java
+++ src/main/java/seng202/team6/analysis/HealthConcernChecker.java
@@ -10,17 +10,18 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 /**
- * This class implements HealthConcernChecker and checks if a user has tachycardia, Bradycardia and
- * cardiovascular mortality.
- * @author
- * @version 1.1, Aug 2018.
+ * This class analyses an activity to check if the user
+ * who participated in the activity is at risk for Tachycardia,
+ * Bradycardia and cardiovascular concerns
  */
 public class HealthConcernChecker {
 
 
     /**
-     *A function which checks if a user is at risk for Tachycardia. Returns true if so, false otherwise.
-//!!     * @param profile The users profile.
+      *A function which checks if a user is at risk for Tachycardia by analysing recorded
+     * heart rates from their activities. Returns true if so, false otherwise.
+     * @param activities an array list of users activities which are being analysed
+     * @param age the age of the user
      * @return A boolean expression for if a user is at risk for Tachycardia.
      */
     public static boolean checkTachycardia(ArrayList<Activity> activities, int age) {
@@ -37,6 +38,14 @@ public class HealthConcernChecker {
     }
 
 
+    /**
+     * A function that determines if the any maximum heart rate of a walking activity, from an
+     * array list of activities is above the minimum threshold for Tachycardia. Returns true
+     * if so, false otherwise.
+     * @param heartRateThreshold the minimum heartRate in which a user is at risk for Tachycardia
+     * @param activities an array list of a users activities
+     * @return a boolean value indicating if a walking heart rate was above the threshold
+     */
     private static boolean checkTachycardiaThreshold(int heartRateThreshold, ArrayList<Activity> activities) {
         for(Activity activity : activities) {
             double maxHeartRate = ActivityAnalysis.findMaximumHeartRate(activity);
@@ -49,8 +58,10 @@ public class HealthConcernChecker {
 
 
     /**
-     * A function which checks if a user is at risk for Bradycardia. Returns true if so, false otherwise.
-//!!     * @param profile The users profile.
+     * A function which checks if a user is at risk for Bradycardia from their
+     * activities. Returns true if so, false otherwise.
+     * @param activities an array list of users activities which are being analysed
+     * @param age the age of the user
      * @return A boolean expression for if a user is at risk for Bradycardia.
      */
     public static boolean checkBradycardia(ArrayList<Activity> activities, int age) {
@@ -65,6 +76,14 @@ public class HealthConcernChecker {
 
     }
 
+
+    /**
+     * Checks if any of a users heart rates for walking activities are below a
+     * given threshold, returning true if so, false otherwise.
+     * @param heartRateThreshold the minimum heart rate indicating a user is at risk for Bradycardia
+     * @param activities an array list of the users activities
+     * @return a boolean value indicating if any recorded walking heart rates were above the threshold
+     */
     private static boolean determineBradycardiaOutcome (double heartRateThreshold, ArrayList<Activity> activities) {
         for (Activity activity : activities) {
             double minimumHeartRate = ActivityAnalysis.findMinimumHeartRate(activity);
@@ -77,9 +96,12 @@ public class HealthConcernChecker {
 
 
     /**
-     * A function which checks if a user is at risk for Cardiovascular Mortaility. Returns true if so, false otherwise.
-//!!     * @param profile The users profile.
-     * @return A boolean expression for if a user is at risk for Bradycardia.
+     * A function which checks if a user may have cardiovascular concerns, buy
+     * determining if any recorded heart rates from their activities are above
+     * a threshold. Returns true if so, false otherwise.
+     * @param activities an array list of users activities which are being analysed
+     * @param age the age of the user
+     * @return A boolean expression for if a user may have cardiovascular concerns.
      */
     public static boolean checkCardiovascularMortality(ArrayList<Activity> activities, int age) {
         if (age >= 18) { // User is an adult
