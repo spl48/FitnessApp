13b725934baa269a8f7229599de2376f90c72b66
Dana Lambert <dla72@uclive.ac.nz>
2018-09-24 14:12:14 +1200

    Deleted more files

--- src/main/java/seng202/team6/datahandling/DatabaseDataWriter.java
+++ src/main/java/seng202/team6/datahandling/DatabaseDataWriter.java
@@ -1,5 +1,5 @@
 package seng202.team6.datahandling;
 
-public class DatabaseDataWriter implements DataWriter {
+public class DatabaseDataWriter {
 
 }
--- src/main/java/seng202/team6/datahandling/FileDataWriter.java
+++
@@ -1,5 +0,0 @@
-package seng202.team6.datahandling;
-
-public class FileDataWriter implements DataWriter {
-
-}
