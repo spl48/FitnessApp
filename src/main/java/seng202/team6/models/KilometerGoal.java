db62fad2d767032d3bf54b55a6d01539b24bf76a
Sean <spl48@uclive.ac.nz>
2018-09-29 14:39:19 +1200

    Added Buttons and Date picker to Goals Screen

--- src/main/java/seng202/team6/models/KilometerGoal.java
+++ src/main/java/seng202/team6/models/KilometerGoal.java
@@ -4,7 +4,29 @@ import java.time.LocalDate;
 
 public class KilometerGoal extends Goal {
 
-    public KilometerGoal() {
-        super("Step Goal", LocalDate.now());
+    private int goalDistance;
+
+    private LocalDate endDate;
+
+    public KilometerGoal(int goalDistance, LocalDate endDate) {
+        super("Kilometer", LocalDate.now());
+        this.goalDistance = goalDistance;
+        this.endDate = endDate;
+    }
+
+    public int getGoalDistance() {
+        return goalDistance;
+    }
+
+    public void setGoalDistance(int goalDistance) {
+        this.goalDistance = goalDistance;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
     }
 }
