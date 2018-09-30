11581bacac79a2f8572dae72ad0eba2307f49485
lem72 <lem72@uclive.ac.nz>
2018-09-30 14:01:33 +1300

    merged user and profile analysis tests

--- src/test/java/seng202/team6/models/UserTest.java
+++ src/test/java/seng202/team6/models/UserTest.java
@@ -104,4 +104,80 @@ public class UserTest extends TestCase {
     public void testGetUserID() {
         assertEquals(56778, user.getUserID());
     }
+
+    public void testCalculateBMI() {
+        User testUser;
+
+        LocalDate date = LocalDate.of(1998, 5, 18);
+        testUser = new User("Joe", "Bloggs",date,"Male", 170.0, 64, "JBloggs", 12345, 50, 100);
+        assertEquals(testUser.calculateBMI(), 22.1, 0.05);   // Average BMI
+
+        testUser.setWeight(2);   //Minimum weight
+        testUser.setHeight(55);  //Minimum height
+        assertEquals(testUser.calculateBMI(), 6.6, 0.05);
+
+        testUser.setWeight(600);  //Maximum weight
+        testUser.setHeight(280);   //Maximum Height
+        assertEquals(testUser.calculateBMI(), 76.5, 0.05);
+
+        // Smallest possible BMI (within the app)
+        testUser.setWeight(2);
+        testUser.setHeight(280);
+        assertEquals(testUser.calculateBMI(), 0.3, 0.05);
+
+        // Largest possible BMI (within the app)
+        testUser.setWeight(600);
+        testUser.setHeight(55);
+        assertEquals(testUser.calculateBMI(), 1983.5, 0.05);
+
+    }
+
+    public void testAnalyseBMI() {
+        User testUser;
+
+        LocalDate date = LocalDate.of(1998, 5, 18);
+        testUser = new User("Joe", "Bloggs",date,"Male", 170.0, 64, "JBloggs", 12345, 50, 100);
+
+        assertEquals("Healthy Weight", testUser.analyseBMI()); // Middle healthy Weight user - BMI of 22.1
+
+        testUser.setWeight(53.5);                                 // Minimum Healthy weight user - BMI of 18.5
+        assertEquals( "Healthy Weight", testUser.analyseBMI());
+
+        testUser.setWeight(71.8);                                 // Maximum Healthy weight user - BMI of 24.8
+        assertEquals( "Healthy Weight", testUser.analyseBMI());
+
+
+
+        testUser.setWeight(30);                                 // Very Under weight user - BMI of 10.4
+        assertEquals( "UnderWeight",testUser.analyseBMI() );
+
+        testUser.setWeight(40);                              // Middle Under weight user - BMI of 13.8
+        assertEquals("UnderWeight", testUser.analyseBMI());
+
+        testUser.setWeight(53.2);                              // Maximum Under weight user - BMI of 18.4
+        assertEquals( "UnderWeight", testUser.analyseBMI());
+
+
+
+        testUser.setWeight(72);                                 // Minimum Over Weight user - BMI of 24.9
+        assertEquals("OverWeight", testUser.analyseBMI());
+
+        testUser.setWeight(79.5);                                 // Middle Over Weight user - BMI of 27.5
+        assertEquals( "OverWeight", testUser.analyseBMI());
+
+        testUser.setWeight(86);                                 // Maximum Over Weight user - BMI of 29.8
+        assertEquals( "OverWeight", testUser.analyseBMI());
+
+
+
+
+        testUser.setWeight(86.5);                                 // Minimum Obese user - BMI of 29.9
+        assertEquals("Obese",testUser.analyseBMI());
+
+        testUser.setWeight(94);                                 // Middle Obese user - BMI of 32.2
+        assertEquals("Obese", testUser.analyseBMI());
+
+        testUser.setWeight(94);                                 // Very Obese user - BMI of 34.9
+        assertEquals("Obese", testUser.analyseBMI());
+    }
 }
