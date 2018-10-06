d0baab0b72d31b298ea434093dd25c626b352793
Angelica Dela Cruz <acd66@uclive.ac.nz>
2018-10-07 11:39:33 +1300

    Changed the message for profile delete pop up.

--- src/main/java/seng202/team6/controller/loginController.java
+++ src/main/java/seng202/team6/controller/loginController.java
@@ -276,7 +276,7 @@ public class loginController extends GeneralScreenController {
 
         delButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
             public void handle(MouseEvent event) {
-                boolean answer = ApplicationManager.getAnswerFromPopUp("Are you sure you want to do this?");
+                boolean answer = ApplicationManager.getAnswerFromPopUp("Are you sure you want to delete this account?");
                 if (answer == true) {
                     databaseManager.getUserWriter().removeUser(username);
                     if (databaseManager.getUserReader().getUsernames().size() > 0) {
