bb6c22c88d2e23e0d7ed63b3f3e04dde7e9b3638
rch141 <rch141@uclive.ac.nz>
2018-10-08 11:24:04 +1300

    window icon

--- src/main/java/seng202/team6/Main.java
+++ src/main/java/seng202/team6/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import seng202.team6.controller.ApplicationManager;
 
@@ -21,6 +22,7 @@ public class Main extends Application {
         primaryStage.show();
         primaryStage.setResizable(false);
         ApplicationManager.initializeApplication();
+        primaryStage.getIcons().add(new Image("/seng202/team6/resources/pics/windowicon.png"));
     }
     public static void main(String[] args) {
         launch(args);
