999f1139301c7a2a519772982feb47feaf5edcea
Angelica Dela Cruz <acd66@uclive.ac.nz>
2018-10-08 15:33:54 +1300

    Javadoc DatabaseUserWriter.

--- src/main/java/seng202/team6/datahandling/DatabaseUserWriter.java
+++ src/main/java/seng202/team6/datahandling/DatabaseUserWriter.java
@@ -9,6 +9,10 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDate;
 
+/**
+ * DatabaseUserWriter handles all the input methods in the database.
+ */
+
 public class DatabaseUserWriter {
     /**
      * The database connection
@@ -77,19 +81,35 @@ public class DatabaseUserWriter {
         }
     }
 
+    /**
+     * A function that updates the first name of the user by the given parameter.
+     * @param firstName A String parameter that represents the first name of the user to be changed to.
+     * @throws SQLException
+     */
     public void updateFirstName(String firstName) throws SQLException {
         String sql = "UPDATE user SET firstname = ? WHERE userid = " + ApplicationManager.getCurrentUserID();
         PreparedStatement updateFirstName = connection.prepareStatement(sql);
         updateFirstName.setString(1, firstName);
         updateFirstName.execute();
     }
+
+    /**
+     * A function that updates the last name of the User by the given parameter.
+     * @param lastName A String parameter that represents the last name of the user to be changed to.
+     * @throws SQLException
+     */
     public void updateLastName(String lastName) throws SQLException {
         String sql = "UPDATE user SET lastname = ? WHERE userid = " + ApplicationManager.getCurrentUserID();
         PreparedStatement updateLastName = connection.prepareStatement(sql);
         updateLastName.setString(1, lastName);
         updateLastName.execute();
-
     }
+
+    /**
+     * A function that updates the user's username by the given parameter.
+     * @param userName A String parameter that represents the username of the user to be changed to.
+     * @throws SQLException
+     */
     public void updateUsername(String userName) throws SQLException {
         String sql = "UPDATE user SET username = ? WHERE userid = " + ApplicationManager.getCurrentUserID();
         PreparedStatement updateUsername = connection.prepareStatement(sql);
@@ -97,25 +117,48 @@ public class DatabaseUserWriter {
         updateUsername.execute();
         ApplicationManager.setCurrentUsername(userName);
     }
+
+    /**
+     * A function that updates the date of birth of the User by the given parameter.
+     * @param dateOfBirth A LocalDate parameter that represents the date of birth to be changed to.
+     * @throws SQLException
+     */
     public void updateDateOfBirth(LocalDate dateOfBirth) throws SQLException {
         String sql = "UPDATE user SET dateofbirth = ? WHERE userid = " + ApplicationManager.getCurrentUserID();
         PreparedStatement updateUsername = connection.prepareStatement(sql);
         updateUsername.setString(1, dateOfBirth.toString());
         updateUsername.execute();
     }
+
+    /**
+     * A function that updates the gender of the user by the given parameter.
+     * @param gender A String parameter that represents the gender of the user to be changed to.
+     * @throws SQLException
+     */
     public void updateGender(String gender) throws SQLException {
         String sql = "UPDATE user SET gender = ? WHERE userid = " + ApplicationManager.getCurrentUserID();
         PreparedStatement updateGender = connection.prepareStatement(sql);
         updateGender.setString(1, gender);
         updateGender.execute();
-
     }
+
+    /**
+     * A function that updates the height of the user by the given parameter.
+     * @param height A Double parameter that represents the height of the user.
+     * @throws SQLException
+     */
     public void updateHeight(double height) throws SQLException {
         String sql = "UPDATE user SET height = ? WHERE userid = " + ApplicationManager.getCurrentUserID();
         PreparedStatement updateHeight = connection.prepareStatement(sql);
         updateHeight.setDouble(1, height);
         updateHeight.execute();
     }
+
+    /**
+     * A function that updates the weight of the user by the given parameter.
+     * @param weight A Double
+     * @throws SQLException
+     */
     public void updateWeight(double weight) throws SQLException {
         String sql = "UPDATE user SET weight = ? WHERE userid = " + ApplicationManager.getCurrentUserID();
         PreparedStatement updateWeight = connection.prepareStatement(sql);
@@ -131,14 +174,30 @@ public class DatabaseUserWriter {
         setUserIntegerProperty(ApplicationManager.getCurrentUserID(), ++logins, "logins");
     }
 
+    /**
+     * A function that sets the distance goal of the user to the given parameter new distance goal.
+     * @param userid An integer that represents the user id
+     * @param newGoal An integer that is used to set the new distance goal into.
+     */
     public void setDistanceGoal(int userid, int newGoal) {
         setUserIntegerProperty(userid, newGoal, "distancegoal");
     }
 
+    /**
+     * A function that sets the steps goal of the user to the given parameter new steps goal.
+     * @param userid An integer that represents the user id
+     * @param newGoal An integer that is used to set the new steps goal into.
+     */
     public void setStepGoal(int userid, int newGoal) {
         setUserIntegerProperty(userid, newGoal, "stepGoal");
     }
 
+    /**
+     * A function that sets the user integer property to the given parameters.
+     * @param userid An integer that represents the user id
+     * @param newGoalValue An integer that represents the new goal value
+     * @param property A String parameter that represents the property type
+     */
     public void setUserIntegerProperty(int userid, int newGoalValue, String property) {
         try {
             String sql = "UPDATE user SET " + property + " = ? WHERE userid = '" + userid + "'";
